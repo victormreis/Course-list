181447ae4f1c1e67b610ee13d1d915895c5c69fa
Victor Martins <vitor_jhoul@hotmail.com>
2022-06-09 15:06:01 -0300

    Criação de rotas e tratamento de erro Criação do component Erro para paginas nao encontradas Criação de rotas e redirecionamento para rotas desejadas.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,7 +3,6 @@ import { Course } from "./course";
 import { courseService } from "./course.service";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
